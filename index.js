49e991d49fdb6a29696b8cc5ec72ebe0451bf6b2
Sacir01 <116180315+Sacir01@users.noreply.github.com>
2023-05-14 19:27:02 +0200

    Commit History Unsuccesfull
    In this commit I have added history path and have changer apikey to student key from openweathermap, unfortunately as can be seen on screeenshoot2.png results are not impresive

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const app = express();
 
+
+const apiKey = 'f2d0d6b6ba5778c2b4be361e9d500d20';
+
+
 app.get('/weather/:latitude/:longitude', async (req, res) => {
-    const apiKey = 'fa8640d2cc45b8d6414a0135ade15513';
+    const apiKey = 'f2d0d6b6ba5778c2b4be361e9d500d20';
     const lat = req.params.latitude;
     const lon = req.params.longitude;
     const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=${apiKey}`;
@@ -12,6 +16,31 @@ app.get('/weather/:latitude/:longitude', async (req, res) => {
   
     res.json(data);
   }); 
+
+  app.get('/weather/:latitude/:longitude/:date', async (req, res) => {
+    const latitude = req.params.latitude;
+    const longitude = req.params.longitude;
+    const date = req.params.date;
+    const apiKey = 'f2d0d6b6ba5778c2b4be361e9d500d20';
+  
+    const url = `https://api.openweathermap.org/data/2.5/onecall/timemachine?lat=${latitude}&lon=${longitude}&dt=${date}&units=metric&appid=${apiKey}`;
+  
+    try {
+      const response = await fetch(url);
+      const data = await response.json();
+  
+      
+  
+      res.json({
+        latitude,
+        longitude,
+        data
+      });
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ error: 'Failed to retrieve historical weather data' });
+    }
+  });
 
   
   const PORT = 3000;

38b68a15f7f62c2101f600637c95803c497fc8a0
Sacir01 <116180315+Sacir01@users.noreply.github.com>
2023-05-15 21:25:59 +0200

    update on history succesfull
    updates history route and now it is succesful, problem was subscription to plan :)

--- index.js
+++ index.js
@@ -17,13 +17,14 @@ app.get('/weather/:latitude/:longitude', async (req, res) => {
     res.json(data);
   }); 
 
-  app.get('/history/:latitude/:longitude/:date', async (req, res) => {
+  app.get('/history/:latitude/:longitude/:month/:day', async (req, res) => {
     const lat = req.params.latitude;
     const lon = req.params.longitude;
-    const date = req.params.date;
-    const apiKey = 'f2d0d6b6ba5778c2b4be361e9d500d20';
+    const month = req.params.month;
+    const day = req.params.day;
+    const apiKey = '10549355dadfb50bfba150f7c13ed615';
   
-    const url = `https://api.openweathermap.org/data/2.5/history/city?lat=${lat}&lon=${lon}&type=hour&start=${date}&cnt=1&appid=${apiKey}`;
+    const url = `https://history.openweathermap.org/data/2.5/aggregated/day?lat=${lat}&lon=${lon}&month=${month}&day=${day}&appid=${apiKey}`;
   
     try {
       const response = await fetch(url);
@@ -32,8 +33,8 @@ app.get('/weather/:latitude/:longitude', async (req, res) => {
       
   
       res.json({
-        latitude,
-        longitude,
+        lat,
+        lon,
         data
       });
     } catch (error) {

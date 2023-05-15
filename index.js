893ece35a67d0a0d4286673e11c628efc8dbeb41
Sacir01 <116180315+Sacir01@users.noreply.github.com>
2023-05-15 11:30:07 +0200

    update index.js
    i have added forecast

--- index.js
+++ index.js
@@ -17,13 +17,13 @@ app.get('/weather/:latitude/:longitude', async (req, res) => {
     res.json(data);
   }); 
 
-  app.get('/weather/:latitude/:longitude/:date', async (req, res) => {
-    const latitude = req.params.latitude;
-    const longitude = req.params.longitude;
+  app.get('/history/:latitude/:longitude/:date', async (req, res) => {
+    const lat = req.params.latitude;
+    const lon = req.params.longitude;
     const date = req.params.date;
     const apiKey = 'f2d0d6b6ba5778c2b4be361e9d500d20';
   
-    const url = `https://api.openweathermap.org/data/2.5/onecall/timemachine?lat=${latitude}&lon=${longitude}&dt=${date}&units=metric&appid=${apiKey}`;
+    const url = `https://api.openweathermap.org/data/2.5/history/city?lat=${lat}&lon=${lon}&type=hour&start=${date}&cnt=1&appid=${apiKey}`;
   
     try {
       const response = await fetch(url);
@@ -39,6 +39,28 @@ app.get('/weather/:latitude/:longitude', async (req, res) => {
     } catch (error) {
       console.error(error);
       res.status(500).json({ error: 'Failed to retrieve historical weather data' });
+    }
+  });
+  app.get('/forecast/:latitude/:longitude', async (req, res) => {
+    const latitude = req.params.latitude;
+    const longitude = req.params.longitude;
+    const apiKey = 'f2d0d6b6ba5778c2b4be361e9d500d20';
+  
+    const url = `https://api.openweathermap.org/data/2.5/forecast/?lat=${latitude}&lon=${longitude}&cnt=4&units=metric&appid=${apiKey}`;
+  
+    try {
+      const response = await fetch(url);
+      const data = await response.json();
+
+  
+      res.json({
+        latitude,
+        longitude,
+        data
+      });
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ error: 'Failed to retrieve weather forecast data' });
     }
   });
 
